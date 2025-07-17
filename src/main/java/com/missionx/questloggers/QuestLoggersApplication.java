b23c2fe23c1437a804d70268a4bff02524e29119
mong9ssi <mong9ssi@naver.com>
2025-07-17 19:25:29 +0900

    Initial commit

---
+++ src/main/java/com/missionx/questloggers/QuestLoggersApplication.java
@@ -0,0 +1,13 @@
+package com.missionx.questloggers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestLoggersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestLoggersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/missionx/questloggers/QuestLoggersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.missionx.questloggers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestLoggersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
