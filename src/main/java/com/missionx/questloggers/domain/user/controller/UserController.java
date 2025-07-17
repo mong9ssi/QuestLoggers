9ed51b020d1a4558b7e63ff9783f808239260f30
mong9ssi <mong9ssi@naver.com>
2025-07-17 20:56:13 +0900

    dd

--- src/main/java/com/missionx/questloggers/domain/user/controller/UserController.java
+++ src/main/java/com/missionx/questloggers/domain/user/controller/UserController.java
@@ -1,4 +1,4 @@
 package com.missionx.questloggers.domain.user.controller;
 
-public class Controller {
+public class UserController {
 }
--- src/main/java/com/missionx/questloggers/domain/user/service/UserService.java
+++ src/main/java/com/missionx/questloggers/domain/user/service/UserService.java
@@ -1,4 +1,4 @@
 package com.missionx.questloggers.domain.user.service;
 
-public class Service {
+public class UserService {
 }
