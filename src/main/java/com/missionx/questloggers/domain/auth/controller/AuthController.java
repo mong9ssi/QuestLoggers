72123b0f3eec14656578a4b77bb3e1a6ee12031f
mong9ssi <mong9ssi@naver.com>
2025-07-18 11:37:36 +0900

    기본 환경 세팅

--- src/main/java/com/missionx/questloggers/domain/auth/controller/AuthController.java
+++ src/main/java/com/missionx/questloggers/domain/auth/controller/AuthController.java
@@ -1,4 +1,15 @@
 package com.missionx.questloggers.domain.auth.controller;
 
+import com.missionx.questloggers.domain.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
 }
--- src/main/java/com/missionx/questloggers/domain/auth/service/AuthService.java
+++ src/main/java/com/missionx/questloggers/domain/auth/service/AuthService.java
@@ -1,4 +1,12 @@
 package com.missionx.questloggers.domain.auth.service;
 
+import com.missionx.questloggers.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserService userService;
 }
